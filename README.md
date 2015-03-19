cb3d4c0a2a9e19336481ebe7b8eff137650cb364
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2015-03-19 11:50:00 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+To install:
+
+`bower install --save ie-location-origin-polyfill`
+
+To use include polyfill before other scripts depending on it:
+
+`<script src='/path_to_bower_components/ie-location-origin-polyfill.js'`
---
+++ angular-min-max-validation.js
@@ -0,0 +1,42 @@
+// credits go to angular team
+
+angular.module('common').directive('yaMin', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            var minVal;
+            ctrl.$validators.min = function(value) {
+                return ctrl.$isEmpty(value) || angular.isUndefined(minVal) || value >= minVal;
+            };
+            scope.$watch(attrs.yaMin, function(value) {
+                if (angular.isDefined(value) && !angular.isNumber(value)) {
+                    value = parseFloat(value, 10);
+                }
+                minVal = angular.isNumber(value) && !isNaN(value) ? value : undefined;
+                ctrl.$validate();
+            });
+        }
+    };
+});
+
+angular.module('common').directive('yaMax', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            var maxValue;
+            ctrl.$validators.max = function(value) {
+                return ctrl.$isEmpty(value) || angular.isUndefined(maxValue) || value <= maxValue;
+            };
+
+            scope.$watch(attrs.yaMax, function(value) {
+                if (angular.isDefined(value) && !angular.isNumber(value)) {
+                    value = parseFloat(value, 10);
+                }
+                maxValue = angular.isNumber(value) && !isNaN(value) ? value : undefined;
+                ctrl.$validate();
+            });
+        }
+    };
+});
